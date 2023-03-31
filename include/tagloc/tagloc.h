813d968e5a1fd306b5cf7c5652f947998d887c65
nyanklc <noyanerdinkilic@gmail.com>
2023-03-31 09:27:46 +0300

    initial commit

---
+++ include/tagloc/tagloc.h
@@ -0,0 +1,9 @@
+#ifndef __TAGLOC_H_
+#define __TAGLOC_H_
+
+#include <ros/ros.h>
+
+namespace tagloc {
+}
+
+#endif
---
+++ src/tagloc_node.cpp
@@ -0,0 +1,16 @@
+#include <tagloc/tagloc.h>
+
+using namespace tagloc;
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "tagloc_node");
+  ros::NodeHandle nh;
+
+  ros::Rate r(20);
+  while (ros::ok()) {
+    ros::spinOnce();
+    r.sleep();
+  }
+
+  return 0;
+}
